2860264b282686a9f470951f872771399915a11b
Gokul Swamy <gokul.swamy@berkeley.edu>
2017-01-02 17:08:04 -0800

    Initial Commit

---
+++ WebTouch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebTouch
+//
+//  Created by Gokul Swamy on 1/2/17.
+//  Copyright © 2017 Gokul Swamy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
